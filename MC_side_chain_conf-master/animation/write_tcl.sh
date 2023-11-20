a820765ef7313eb1292841dfc15304f7af40e308
mshanturov <92581780+mshanturov@users.noreply.github.com>
2023-11-20 04:52:08 +0300

    Delete MC_side_chain_conf-master directory

--- MC_side_chain_conf-master/animation/write_tcl.sh
+++
@@ -1,3 +0,0 @@
-codedir=C:/Users/79199/PycharmProjects/MC_side_chain_conf/animation
-sed "s/YYY/$1/" $codedir/script_ex.tcl > $codedir/script_temp.tcl
-sed "s/XXX/$2/" $codedir/script_temp.tcl >> $codedir/script.tcl
