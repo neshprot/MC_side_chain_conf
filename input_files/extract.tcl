2c37b8cb067bf5a04ab53c060bd19ff3b43ac2a5
mshanturov <92581780+mshanturov@users.noreply.github.com>
2024-03-21 16:26:46 +0300

    Update extract.tcl

--- input_files/extract.tcl
+++ input_files/extract.tcl
@@ -3,5 +3,5 @@ mol addfile d95n_0_working_min-0.dcd waitfor all
 set a [atomselect top protein]
 set nf [molinfo top get numframes]
 set res [ expr $nf - 1 ]
-animate write pdb {d95n_0_working.pdb} beg $res end $res sel $a
+animate write pdb {out.pdb} beg $res end $res sel $a
 exit
